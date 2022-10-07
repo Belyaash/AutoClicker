63883da2215ceb57cbcb3798e8e4bc53e8a0e5b0
BelyaevAndrey <andrew205103@gmail.com>
2022-10-07 03:45:26 +0300

    Первая версия автокликера

--- AutoClicker/Form1.Designer.cs
+++ AutoClicker/Form1.Designer.cs
@@ -28,36 +28,225 @@ partial class Form1
     /// </summary>
     private void InitializeComponent()
     {
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Interval = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.GroupBoxLeftRight = new System.Windows.Forms.GroupBox();
+            this.RightButton = new System.Windows.Forms.RadioButton();
+            this.LeftButton = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.DoubleButton = new System.Windows.Forms.RadioButton();
+            this.OneButton = new System.Windows.Forms.RadioButton();
+            this.SetCursorPosition = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.XPosition = new System.Windows.Forms.TextBox();
+            this.YPosition = new System.Windows.Forms.TextBox();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.StopButton = new System.Windows.Forms.Button();
+            this.GroupBoxLeftRight.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // textBox1
+            // Interval
             // 
-            this.textBox1.Location = new System.Drawing.Point(285, 20);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(126, 27);
-            this.textBox1.TabIndex = 0;
+            this.Interval.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Interval.Location = new System.Drawing.Point(278, 20);
+            this.Interval.MaxLength = 12;
+            this.Interval.Name = "Interval";
+            this.Interval.Size = new System.Drawing.Size(162, 32);
+            this.Interval.TabIndex = 0;
+            this.Interval.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Interval_KeyPress);
+            this.Interval.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Interval_KeyUp);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(35, 20);
+            this.label1.Location = new System.Drawing.Point(28, 20);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(244, 23);
             this.label1.TabIndex = 1;
             this.label1.Text = "Set clicker interval in ms";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(28, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(161, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Set click options";
+            // 
+            // GroupBoxLeftRight
+            // 
+            this.GroupBoxLeftRight.Controls.Add(this.RightButton);
+            this.GroupBoxLeftRight.Controls.Add(this.LeftButton);
+            this.GroupBoxLeftRight.Location = new System.Drawing.Point(22, 128);
+            this.GroupBoxLeftRight.Name = "GroupBoxLeftRight";
+            this.GroupBoxLeftRight.Size = new System.Drawing.Size(182, 45);
+            this.GroupBoxLeftRight.TabIndex = 3;
+            this.GroupBoxLeftRight.TabStop = false;
+            // 
+            // RightButton
+            // 
+            this.RightButton.AutoSize = true;
+            this.RightButton.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.RightButton.Location = new System.Drawing.Point(79, 11);
+            this.RightButton.Name = "RightButton";
+            this.RightButton.Size = new System.Drawing.Size(82, 27);
+            this.RightButton.TabIndex = 1;
+            this.RightButton.Text = "Right";
+            this.RightButton.UseVisualStyleBackColor = true;
+            // 
+            // LeftButton
+            // 
+            this.LeftButton.AutoSize = true;
+            this.LeftButton.Checked = true;
+            this.LeftButton.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.LeftButton.Location = new System.Drawing.Point(6, 11);
+            this.LeftButton.Name = "LeftButton";
+            this.LeftButton.Size = new System.Drawing.Size(69, 27);
+            this.LeftButton.TabIndex = 0;
+            this.LeftButton.TabStop = true;
+            this.LeftButton.Text = "Left";
+            this.LeftButton.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.DoubleButton);
+            this.groupBox1.Controls.Add(this.OneButton);
+            this.groupBox1.Location = new System.Drawing.Point(22, 172);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(182, 45);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            // 
+            // DoubleButton
+            // 
+            this.DoubleButton.AutoSize = true;
+            this.DoubleButton.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.DoubleButton.Location = new System.Drawing.Point(79, 11);
+            this.DoubleButton.Name = "DoubleButton";
+            this.DoubleButton.Size = new System.Drawing.Size(96, 27);
+            this.DoubleButton.TabIndex = 1;
+            this.DoubleButton.Text = "Double";
+            this.DoubleButton.UseVisualStyleBackColor = true;
+            // 
+            // OneButton
+            // 
+            this.OneButton.AutoSize = true;
+            this.OneButton.Checked = true;
+            this.OneButton.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.OneButton.Location = new System.Drawing.Point(6, 11);
+            this.OneButton.Name = "OneButton";
+            this.OneButton.Size = new System.Drawing.Size(69, 27);
+            this.OneButton.TabIndex = 0;
+            this.OneButton.TabStop = true;
+            this.OneButton.Text = "One";
+            this.OneButton.UseVisualStyleBackColor = true;
+            // 
+            // SetCursorPosition
+            // 
+            this.SetCursorPosition.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.SetCursorPosition.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.SetCursorPosition.Location = new System.Drawing.Point(210, 96);
+            this.SetCursorPosition.Name = "SetCursorPosition";
+            this.SetCursorPosition.Size = new System.Drawing.Size(241, 34);
+            this.SetCursorPosition.TabIndex = 5;
+            this.SetCursorPosition.Text = "Set cursor position (F3)";
+            this.SetCursorPosition.UseVisualStyleBackColor = false;
+            this.SetCursorPosition.Click += new System.EventHandler(this.SetCursorPosition_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(280, 150);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 23);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "X";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(376, 150);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(24, 23);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Y";
+            // 
+            // XPosition
+            // 
+            this.XPosition.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.XPosition.Location = new System.Drawing.Point(247, 185);
+            this.XPosition.Name = "XPosition";
+            this.XPosition.ReadOnly = true;
+            this.XPosition.Size = new System.Drawing.Size(90, 32);
+            this.XPosition.TabIndex = 8;
+            this.XPosition.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // YPosition
+            // 
+            this.YPosition.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.YPosition.Location = new System.Drawing.Point(343, 185);
+            this.YPosition.Name = "YPosition";
+            this.YPosition.ReadOnly = true;
+            this.YPosition.Size = new System.Drawing.Size(90, 32);
+            this.YPosition.TabIndex = 8;
+            this.YPosition.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // StartButton
+            // 
+            this.StartButton.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.StartButton.Location = new System.Drawing.Point(22, 247);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(182, 61);
+            this.StartButton.TabIndex = 9;
+            this.StartButton.Text = "Start(F2)";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // StopButton
+            // 
+            this.StopButton.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.StopButton.Location = new System.Drawing.Point(247, 247);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(180, 61);
+            this.StopButton.TabIndex = 10;
+            this.StopButton.Text = "Stop(F12)";
+            this.StopButton.UseVisualStyleBackColor = true;
+            this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(423, 380);
+            this.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.ClientSize = new System.Drawing.Size(452, 324);
+            this.Controls.Add(this.StopButton);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.YPosition);
+            this.Controls.Add(this.XPosition);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.SetCursorPosition);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.GroupBoxLeftRight);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Interval);
+            this.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "AutoClicker";
+            this.GroupBoxLeftRight.ResumeLayout(false);
+            this.GroupBoxLeftRight.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -65,6 +254,20 @@ partial class Form1
 
     #endregion
 
-    private TextBox textBox1;
+    private TextBox Interval;
     private Label label1;
+    private Label label2;
+    private GroupBox GroupBoxLeftRight;
+    private RadioButton RightButton;
+    private RadioButton LeftButton;
+    private GroupBox groupBox1;
+    private RadioButton DoubleButton;
+    private RadioButton OneButton;
+    private Button SetCursorPosition;
+    private Label label3;
+    private Label label4;
+    private TextBox XPosition;
+    private TextBox YPosition;
+    private Button StartButton;
+    private Button StopButton;
 }
--- AutoClicker/Form1.cs
+++ AutoClicker/Form1.cs
@@ -1,9 +1,224 @@
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using AutoClicker.Annotations;
+
 namespace AutoClicker;
 
-public partial class Form1 : Form
+public partial class Form1 : Form, INotifyPropertyChanged
 {
+    private bool _isStart = false;
+    private int _period = 10000;
+
+    public int Period
+    {
+        get => _period;
+        set
+        {
+            _period = value switch
+            {
+                < 100 => 100,
+                _ => value
+            };
+            OnPropertyChanged(nameof(Period));
+        }
+    }
+
+
+    private int _x = 0;
+    public int X
+    {
+        get => _x;
+        set
+        {
+            _x = value;
+            OnPropertyChanged(nameof(X));
+        }
+    }
+
+    private int _y = 0;
+    public int Y
+    {
+        get => _y;
+        set
+        {
+            _y = value;
+            OnPropertyChanged(nameof(Y));
+        }
+    }
+
+    #region MouseHooks
+
+    [DllImport("user32.dll")]
+    static extern bool GetCursorPos(out Point pt);
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+    public static extern void mouse_event(uint dwFlags, int dx, int dy, int dwData, int dwExtraInfo);
+
+    private const int MOUSEEVENTF_ABSOLUTE = 0x8000;
+    private const int MOUSEEVENTF_LEFTDOWN = 0x0002;
+    private const int MOUSEEVENTF_LEFTUP = 0x0004;
+    private const int MOUSEEVENTF_RIGHTDOWN = 0x0008;
+    private const int MOUSEEVENTF_RIGHTUP = 0x0010;
+    private const int MOUSEEVENTF_MOVE = 0x0001;
+
+    #endregion
+
+    #region KeyBoardHooks
+
+    private const int WH_KEYBOARD_LL = 13;
+    private LowLevelKeyboardProcDelegate m_callback;
+    private IntPtr m_hHook;
+
+    [DllImport("user32.dll", SetLastError = true)]
+    private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProcDelegate lpfn, IntPtr hMod,
+        int dwThreadId);
+
+    [DllImport("user32.dll", SetLastError = true)]
+    private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+    [DllImport("Kernel32.dll", SetLastError = true)]
+    private static extern IntPtr GetModuleHandle(IntPtr lpModuleName);
+
+    [DllImport("user32.dll", SetLastError = true)]
+    private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+    private IntPtr LowLevelKeyboardHookProc(int nCode, IntPtr wParam, IntPtr lParam)
+    {
+        if (nCode >= 0)
+        {
+            var khs = (KeyboardHookStruct)
+                Marshal.PtrToStructure(lParam,
+                    typeof(KeyboardHookStruct));
+            if (Convert.ToInt32("" + wParam) == 256)
+            {
+                if ((int)khs.VirtualKeyCode == 123) //F12
+                {
+                    StopButton.PerformClick();
+                    WindowState = FormWindowState.Normal;
+                }
+
+                if ((int)khs.VirtualKeyCode == 113) //F2
+                {
+                    StartButton.PerformClick();
+                    WindowState = FormWindowState.Minimized;
+                }
+
+                if ((int)khs.VirtualKeyCode == 114) //F3
+                {
+                    SetCursorPosition.PerformClick();
+                    WindowState = FormWindowState.Normal;
+                }
+            }
+        }
+
+        return CallNextHookEx(m_hHook, nCode, wParam, lParam);
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    private struct KeyboardHookStruct
+    {
+        public readonly int VirtualKeyCode;
+        public readonly int ScanCode;
+        public readonly int Flags;
+        public readonly int Time;
+        public readonly IntPtr ExtraInfo;
+    }
+
+    private delegate IntPtr LowLevelKeyboardProcDelegate(
+        int nCode, IntPtr wParam, IntPtr lParam);
+
+    public void SetHook()
+    {
+        m_callback = LowLevelKeyboardHookProc;
+        m_hHook = SetWindowsHookEx(WH_KEYBOARD_LL,
+            m_callback,
+            GetModuleHandle(IntPtr.Zero), 0);
+    }
+    public void Unhook()
+    {
+        UnhookWindowsHookEx(m_hHook);
+    }
+
+
+    #endregion
+
+
     public Form1()
     {
         InitializeComponent();
+        XPosition.DataBindings.Add("Text", this, nameof(X));
+        YPosition.DataBindings.Add("Text", this, nameof(Y));
+        Interval.DataBindings.Add("Text", this, nameof(Period),
+            true, DataSourceUpdateMode.OnPropertyChanged);
+        SetHook();
+    }
+
+    private void Interval_KeyPress(object sender, KeyPressEventArgs e)
+    {
+        e.Handled = (!char.IsDigit(e.KeyChar) && (!char.IsControl(e.KeyChar)));
+    }
+
+    private void Interval_KeyUp(object sender, KeyEventArgs e)
+    {
+        OnPropertyChanged(nameof(Period));
+    }
+
+    private void StopButton_Click(object sender, EventArgs e)
+    {
+        _isStart = false;
+    }
+
+    private void SetCursorPosition_Click(object sender, EventArgs e)
+    {
+        GetCursorPos(out Point pt);
+        X = pt.X;
+        Y = pt.Y;
+    }
+
+    private async void StartButton_Click(object sender, EventArgs e)
+    {
+        _isStart = true;
+        Size res = Screen.PrimaryScreen.Bounds.Size;
+        int x = 65535 / res.Width * X;
+        int y = 65535 / res.Height * Y;
+
+        while (_isStart)
+        {
+            int clicks = OneButton.Checked ? 1 : 2;
+            uint mouseClickDownType;
+            uint mouseClickUpType;
+
+            if (LeftButton.Checked)
+            {
+                mouseClickDownType = MOUSEEVENTF_LEFTDOWN;
+                mouseClickUpType = MOUSEEVENTF_LEFTUP;
+            }
+            else
+            {
+                mouseClickDownType = MOUSEEVENTF_RIGHTDOWN;
+                mouseClickUpType = MOUSEEVENTF_RIGHTUP;
+            }
+
+            mouse_event(MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_MOVE,
+                x, y, 0, 0);
+
+            for (int i = 0; i < clicks; i++)
+            {
+                mouse_event(MOUSEEVENTF_ABSOLUTE | mouseClickDownType | mouseClickUpType,
+                    x, y, 0, 0);
+            }
+
+            await Task.Delay(Period);
+        }
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    [NotifyPropertyChangedInvocator]
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
